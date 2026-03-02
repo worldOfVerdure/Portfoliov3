b67d93e6e193b217a0b2e7cf687441d89461ce5b
worldOfVerdure <perspicaciousdotard@gmail.com>
2026-03-01 21:45:42 -0500

    Bug that ContactForm defaults control label text color

--- src/components/test/testContactForm/TestContactForm.tsx
+++ src/components/test/testContactForm/TestContactForm.tsx
@@ -1,11 +1,27 @@
 import { ContactForm } from "../../elevated/ContactForm";
 import { SectionHeading } from "../reuseables";
+import styles from "./styles/testContactForm.module.css";
 
 export const TestContactForm = () => {
   return (
-    <>
+    <section className={styles.testContactSection}>
       <SectionHeading>Contact Form</SectionHeading>
-      <ContactForm />
-    </>
+      <ContactForm/>
+    </section>
   );
 }
+
+/*
+!zzz
+ classes={{
+          form: styles.testContactForm,
+          fieldset: styles.testContactFieldset,
+          legend: styles.testContactLegend,
+          control: styles.testContactControl,
+          textarea: styles.testContactTextarea
+        }}
+
+  This is one base form, use classes to style base.
+  Can make additional bases.
+
+*/
