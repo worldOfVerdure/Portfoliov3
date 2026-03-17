2bf004bf9b9687337e20aaf793c9f3cd50f7faa2
worldOfVerdure <perspicaciousdotard@gmail.com>
2026-03-17 15:43:09 -0400

    Added animations to Hero page

--- src/components/project/hero/Hero.tsx
+++ src/components/project/hero/Hero.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { useEffect, useState } from 'react';
 import { ChevronDownIcon } from '@radix-ui/react-icons';
 import { Link } from '../../primitives/Link';
 import { Stack } from '../../primitives/Stack';
 import styles from './styles/Hero.module.css';
 
 export const Hero = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <section className={`${styles.heroContainer} full-width`} >
       <Stack className={styles.heroStack}>
@@ -11,11 +20,11 @@ export const Hero = () => {
           <h1 className={`${styles.heroH1} text-center`}>
             Hello, I am <span className={styles.heroH1Name}>Andrew Chupka</span>
           </h1>
-          <h2 className={`${styles.heroH2} text-center`}>I'm a Fullstack Developer</h2>
+          <h2 className={`${styles.heroH2} text-center`}>I&apos;m a Fullstack Developer</h2>
         </div>
         <Link
           classes={{ label: styles.heroLinkLabel }}
-          className={styles.heroLink}
+          className={`${styles.heroLink} ${isMounted ? styles.heroLinkEntered : ''}`}
           href="#projects"
           size="lg"
           variant="buttonGhost"
