5e2e95625cf3717f7924da42d8c5b4d7f4382299
worldOfVerdure <perspicaciousdotard@gmail.com>
2026-03-21 14:00:02 -0400

    Add a landscape media query to position Hero content down

--- src/components/project/hero/Hero.tsx
+++ src/components/project/hero/Hero.tsx
@@ -18,13 +18,13 @@ export const Hero = () => {
         data-nav-theme="dark"
         data-nav-theme-sentinel
       />
-      <Stack className={styles.heroStack}>
-        <div>
+      <Stack className={styles.heroStack} gap="var(--space-5)" >
+        {/* <div> */}
           <h1 className={`${styles.heroH1} font-header text-center`}>
             I&apos;m <span className={styles.heroH1Name}>Andrew Chupka</span>
           </h1>
           <h2 className={`${styles.heroH2} font-header text-center`}>I&apos;m a Fullstack Developer</h2>
-        </div>
+        {/* </div> */}
         <Link
           classes={{ label: styles.heroLinkLabel }}
           className={styles.heroLink}
--- src/components/project/projects/Projects.tsx
+++ src/components/project/projects/Projects.tsx
@@ -3,7 +3,7 @@ import styles from './styles/projects.module.css';
 export const Projects = () => {
   return (
     <section className={`${styles.projectsContainer} full-width`} id="projects">
-      
+      <h2 className="sectionH2">Projects</h2>
     </section>
   );
 }
