6346d20f97ede7b5b1632635143c44810a32b15b
worldOfVerdure <perspicaciousdotard@gmail.com>
2026-03-22 12:18:09 -0400

    update[Image]: use 16:9 aspect ratio for project images

--- src/components/project/projects/projectsData/projectsData.ts
+++ src/components/project/projects/projectsData/projectsData.ts
@@ -22,8 +22,8 @@ export const projectsData: ProjectCardProps[] = [
     projectTitle: 'Simple Template',
     imgSrc: simpleTemplateImg,
     imgAlt: 'A thumbnail of the Simple Template project.',
-    imgHeight: 936,
-    imgWidth: 1423,
+    imgHeight: 939,
+    imgWidth: 1670,
     liveProject: 'https://simple-template-gamma.vercel.app/',
     github: 'https://github.com/worldOfVerdure/simpleTemplate',
     description: 'Description of Project 1'
@@ -32,8 +32,8 @@ export const projectsData: ProjectCardProps[] = [
     projectTitle: 'Animated D3.js Graph',
     imgSrc: animatedD3JSImg,
     imgAlt: 'A thumbnail of the Animated D3.js Graph project.',
-    imgHeight: 794,
-    imgWidth: 1136,
+    imgHeight: 840,
+    imgWidth: 1493,
     liveProject: 'https://animated-graph-simulation-d3.vercel.app/',
     github: 'https://github.com/worldOfVerdure/animatedGraphSimulationD3',
     description: 'Description of Project 2'
@@ -42,8 +42,8 @@ export const projectsData: ProjectCardProps[] = [
     projectTitle: 'Match Game',
     imgSrc: matchGameImg,
     imgAlt: 'A thumbnail of the Match Game project.',
-    imgHeight: 926,
-    imgWidth: 1877,
+    imgHeight: 937,
+    imgWidth: 1665,
     liveProject: 'https://worldofverdure.github.io/GameNMatch/',
     github: 'https://github.com/worldOfVerdure/GameNMatch',
     description: 'Description of Project 3'
--- src/components/project/projects/reuseables/projectCard/ProjectCard.tsx
+++ src/components/project/projects/reuseables/projectCard/ProjectCard.tsx
@@ -21,13 +21,15 @@ export const ProjectCard = ({
   return (
     <Stack align="center" gap="var(--spacing-3)" >
       <h3>{projectTitle}</h3>
-      <Image
-        alt={imgAlt}
-        className={styles.projectImg}
-        height={imgHeight}
-        src={imgSrc}
-        width={imgWidth}
-      />
+      <div className={styles.projectImageContainer} >
+        <Image
+          alt={imgAlt}
+          className={styles.projectImg}
+          height={imgHeight}
+          src={imgSrc}
+          width={imgWidth}
+        />
+      </div>
       <Stack direction="row" gap="var(--spacing-2)" >
         <Link href={liveProject} variant="buttonPrimary" >Live Project</Link>
         <Link href={github} variant="buttonSecondary" >GitHub</Link>
