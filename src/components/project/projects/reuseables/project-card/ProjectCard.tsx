068a402449fae3d839875e4f562b0f804915ce16
worldOfVerdure <perspicaciousdotard@gmail.com>
2026-03-30 21:57:58 -0400

    [Remove utility class]: deleted utility class text-center from p element in ProjectCard.tsx

--- src/components/project/projects/reuseables/project-card/ProjectCard.tsx
+++ src/components/project/projects/reuseables/project-card/ProjectCard.tsx
@@ -69,7 +69,7 @@ export const ProjectCard = ({
        :
         <Stack align="center" gap="var(--space-4)" >
           <h3 className={`${styles.projectsH3} full-width text-center`}>{projectTitle}</h3>
-          <p className={`${styles.projectDescription} text-center`}>{description}</p>
+          <p className={`${styles.projectDescription}`}>{description}</p>
           <Link className={styles.projectImageContainer} href={liveProject} unstyled>
             <Image
               alt={imgAlt}
