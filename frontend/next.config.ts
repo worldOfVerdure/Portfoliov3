9d120995f92c163aafe025934d5e1f364b209b74
worldOfVerdure <contact@andrewchupka.com>
2026-04-03 21:00:06 -0400

    Remove warning by editing .next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
-  turbopack: {
-    root: process.cwd(),
-  },
 };
 
 export default nextConfig;
